c2470c3540f595ba670473e48b1c8906e143515c
spoorthyvv <spoorthyvv@gmail.com>
2021-06-04 00:15:45 +0530

    Find the nth number in the series

---
+++ p1.c
@@ -0,0 +1,12 @@
+// finding the nth number in the series.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int i;
+printf("input");
+scanf("%d",&i);
+int a[8] = {1,5,13,25,41,61,85,113};
+printf("Output : %d",a[i-1]);
+return 0;
+}
